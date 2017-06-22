d3596c39a65fdd550437f2361f610b07c6da0511
mpppk <niboshiporipori@gmail.com>
2017-06-23 08:33:12 +0900

    Open editor for editing pull request message

--- cmd/create_pullrequest.go
+++ cmd/create_pullrequest.go
@@ -1,7 +1,18 @@
 package cmd
 
 import (
+	"os"
+	"os/exec"
+
+	"io/ioutil"
+
 	"fmt"
+
+	"bufio"
+	"bytes"
+	"io"
+	"regexp"
+	"strings"
 
 	"github.com/mpppk/hlb/etc"
 	"github.com/mpppk/hlb/git"
@@ -9,23 +20,70 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	DEFAULT_BRANCH_NAME = "master"
+)
+
+func readTitleAndmessage(reader io.Reader, cs string) (title, body string, err error) {
+	var titleParts, bodyParts []string
+
+	r := regexp.MustCompile("\\S")
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, cs) {
+			continue
+		}
+
+		if len(bodyParts) == 0 && r.MatchString(line) {
+			titleParts = append(titleParts, line)
+		} else {
+			bodyParts = append(bodyParts, line)
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return
+	}
+
+	title = strings.Join(titleParts, " ")
+	title = strings.TrimSpace(title)
+
+	body = strings.Join(bodyParts, "\n")
+	body = strings.TrimSpace(body)
+
+	return
+}
+
 var createpullrequestCmd = &cobra.Command{
 	Use:   "pull-request",
 	Short: "Create pull request",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
+		pullreqFileName := "PULLREQ_EDITMSG"
+		c := exec.Command("vim", pullreqFileName)
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		c.Run()
+
+		contents, err := ioutil.ReadFile(pullreqFileName)
+		etc.PanicIfErrorExist(err)
+
+		err = os.Remove(pullreqFileName)
+		etc.PanicIfErrorExist(err)
+
+		title, message, err := readTitleAndmessage(bytes.NewReader(contents), "#")
+		etc.PanicIfErrorExist(err)
+
 		base, err := hlblib.NewCmdBase()
 		etc.PanicIfErrorExist(err)
 		sw := hlblib.ClientWrapper{Base: base}
 
-		title := "Sample title"
-		message := "Sample message"
 		baseOwner := base.Remote.Owner
-		baseBranch := "master"
+		baseBranch := DEFAULT_BRANCH_NAME
 		headBranch, err := git.GetCurrentBranch(".")
-
-		git.GetCurrentBranch(".")
 
 		pr, err := sw.CreatePullRequest(baseOwner, baseBranch, headBranch, title, message)
 		etc.PanicIfErrorExist(err)
