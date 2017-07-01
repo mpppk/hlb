b101230107e67578be5ff13586437984b45d9427
mpppk <niboshiporipori@gmail.com>
2017-07-01 12:47:46 +0900

    Add test of pull-request creating for gitlab client

--- gitlab/gitlab_service_test.go
+++ gitlab/gitlab_service_test.go
@@ -3,6 +3,10 @@ package gitlab
 import (
 	"testing"
 
+	"fmt"
+
+	"context"
+
 	"github.com/mpppk/hlb/etc"
 	"github.com/xanzy/go-gitlab"
 )
@@ -52,6 +56,15 @@ func (m *MockMergeRequestsService) ListMergeRequests(pid interface{}, opt *gitla
 	}, nil, nil
 }
 
+func (m *MockMergeRequestsService) CreateMergeRequest(pid interface{}, opt *gitlab.CreateMergeRequestOptions, options ...gitlab.OptionFunc) (*gitlab.MergeRequest, *gitlab.Response, error) {
+	mrIID := 2
+	return &gitlab.MergeRequest{
+		IID:    mrIID,
+		Title:  *opt.Title,
+		WebURL: fmt.Sprintf("https://gitlab.com/%v/%v/merge_requests/%v", "testuser", "newrepo", mrIID),
+	}, nil, nil
+}
+
 type MockRawClient struct {
 	Projects      *MockProjectsService
 	Issues        *MockIssuesService
@@ -86,18 +99,24 @@ func newMockRawClient() *MockRawClient {
 }
 
 type Client_GetRepositoryURLTest struct {
-	serviceConfig            *etc.ServiceConfig
-	rawClient                rawClient
-	willBeError              bool
-	user                     string
-	repo                     string
-	issueID                  int
-	pullRequestID            int
-	expectedRepositoryURL    string
-	expectedIssuesURL        string
-	expectedIssueURL         string
-	expectedMergeRequestsURL string
-	expectedPullRequestURL   string
+	serviceConfig                     *etc.ServiceConfig
+	rawClient                         rawClient
+	willBeError                       bool
+	user                              string
+	repo                              string
+	createRepo                        string
+	createPRTitle                     string
+	createPRMessage                   string
+	issueID                           int
+	pullRequestID                     int
+	expectedRepositoryURL             string
+	expectedIssuesURL                 string
+	expectedIssueURL                  string
+	expectedMergeRequestsURL          string
+	expectedPullRequestURL            string
+	expectedCreatedPullRequestURL     string
+	expectedCreatedPullRequestTitle   string
+	expectedCreatedPullRequestMessage string
 }
 
 type Util struct {
@@ -114,7 +133,7 @@ func (u *Util) printErrorIfUnexpected(err error, msg string) bool {
 	return ok
 }
 
-func (u *Util) assertString(expected, actual string, msg string) bool {
+func (u *Util) assertString(actual, expected string, msg string) bool {
 	ok := actual == expected
 	if !ok {
 		u.t.Errorf("%v: Expected %v: %v, Actual: %v",
@@ -150,18 +169,24 @@ func TestClient_GetRepositoryURL(t *testing.T) {
 			repo:          "",
 		},
 		{
-			serviceConfig:            serviceConfig,
-			rawClient:                mockRawClient,
-			willBeError:              false,
-			user:                     "testuser",
-			repo:                     "testrepo",
-			issueID:                  1,
-			pullRequestID:            1,
-			expectedRepositoryURL:    "https://gitlab.com/testuser/testrepo",
-			expectedIssuesURL:        "https://gitlab.com/testuser/testrepo/issues",
-			expectedIssueURL:         "https://gitlab.com/testuser/testrepo/issues/1",
-			expectedMergeRequestsURL: "https://gitlab.com/testuser/testrepo/merge_requests",
-			expectedPullRequestURL:   "https://gitlab.com/testuser/testrepo/merge_requests/1",
+			serviceConfig:                     serviceConfig,
+			rawClient:                         mockRawClient,
+			willBeError:                       false,
+			user:                              "testuser",
+			repo:                              "testrepo",
+			createRepo:                        "newrepo",
+			createPRTitle:                     "New Pull Request",
+			createPRMessage:                   "New Message",
+			issueID:                           1,
+			pullRequestID:                     1,
+			expectedRepositoryURL:             "https://gitlab.com/testuser/testrepo",
+			expectedIssuesURL:                 "https://gitlab.com/testuser/testrepo/issues",
+			expectedIssueURL:                  "https://gitlab.com/testuser/testrepo/issues/1",
+			expectedMergeRequestsURL:          "https://gitlab.com/testuser/testrepo/merge_requests",
+			expectedPullRequestURL:            "https://gitlab.com/testuser/testrepo/merge_requests/1",
+			expectedCreatedPullRequestURL:     "https://gitlab.com/testuser/newrepo/merge_requests/2",
+			expectedCreatedPullRequestTitle:   "New Pull Request",
+			expectedCreatedPullRequestMessage: "New Message",
 		},
 	}
 
@@ -204,6 +229,14 @@ func TestClient_GetRepositoryURL(t *testing.T) {
 			continue
 		}
 		util.assertString(pullRequestURL, test.expectedPullRequestURL, title)
+
+		title = "Created MergeRequest"
+		createdPullRequest, err := client.CreatePullRequest(context.Background(), test.user, "master", test.user, "feature", test.createRepo, test.createPRTitle, test.createPRMessage)
+		if ok := util.printErrorIfUnexpected(err, title); ok && err != nil {
+			continue
+		}
+		util.assertString(createdPullRequest.GetHTMLURL(), test.expectedCreatedPullRequestURL, title+" URL")
+		util.assertString(createdPullRequest.GetTitle(), test.expectedCreatedPullRequestTitle, title+" Title")
 
 		if test.willBeError {
 			t.Errorf("%v: Error expected, params: %#v",
